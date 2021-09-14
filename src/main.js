84a346846fc30fa1b557f0fefa620f569f48b563
Joel Clermont <jclermont@gmail.com>
2021-09-14 16:31:31 -0500

    set up an empty store

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import bootstrap from "bootstrap";
 require("bootstrap-icons/font/bootstrap-icons.css");
 import "./scss/main.scss";
 import { createApp } from "vue";
+import store from "./store";
 import App from "./App.vue";
 import router from "./router";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(store).mount("#app");
